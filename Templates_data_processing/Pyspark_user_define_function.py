b605b07a691b0fdd2ffd31efbae123254b645cdb
Yan Wang <53322758+YanWang0408@users.noreply.github.com>
2020-04-28 17:39:26 -0400

    Add files via upload

---
+++ Templates_data_processing/Pyspark_user_define_function.py
@@ -0,0 +1,22 @@
+def reverseString(mystr):
+    return mystr[::-1]
+#Wrap the function and store as a variable
+udfReverseString = udf(reverseString, StringType())
+#use it with spark
+user_df = user_df.withColumn('reverseName', udfReverseString(user_df.Name))
+
+
+
+def getFirstAndMiddle(names):
+  return ' '.join(names[0:2])
+
+# Define the method as a UDF
+udfFirstAndMiddle = F.udf(getFirstAndMiddle, StringType())
+
+# Create a new column using your UDF
+voter_df = voter_df.withColumn('first_and_middle_name', udfFirstAndMiddle(voter_df.splits))
+
+# Drop the unnecessary columns then show the DataFrame
+voter_df = voter_df.drop('first_name')
+voter_df = voter_df.drop('splits')
+voter_df.show()
---
+++ Templates_data_processing/monotonically_increasing_id.py
@@ -0,0 +1,25 @@
+# Select all the unique council voters
+voter_df = df.select(df["VOTER NAME"]).distinct()
+
+# Count the rows in voter_df
+print("\nThere are %d rows in the voter_df DataFrame.\n" % voter_df.count())
+
+# Add a ROW_ID
+voter_df = voter_df.withColumn('ROW_ID', F.monotonically_increasing_id())
+
+# Show the rows with 10 highest IDs in the set
+voter_df.orderBy(voter_df.ROW_ID.desc()).show(10)
+
+
+
+# Print the number of partitions in each DataFrame
+print("\nThere are %d partitions in the voter_df DataFrame.\n" % voter_df.rdd.getNumPartitions())
+print("\nThere are %d partitions in the voter_df_single DataFrame.\n" % voter_df_single.rdd.getNumPartitions())
+
+# Add a ROW_ID field to each DataFrame
+voter_df = voter_df.withColumn('ROW_ID', F.monotonically_increasing_id())
+voter_df_single = voter_df_single.withColumn('ROW_ID', F.monotonically_increasing_id())
+
+# Show the top 10 IDs in each DataFrame 
+voter_df.orderBy(voter_df.ROW_ID.desc()).show(10)
+voter_df_single.orderBy(voter_df_single.ROW_ID.desc()).show(10)
