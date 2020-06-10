3843560fc775fbee61daee72b9d3374f569794c1
Yan Wang <53322758+YanWang0408@users.noreply.github.com>
2020-06-10 17:53:50 -0400

    Update Pyspark_Removing_Invalid_Rows.py

--- Templates_data_processing/Pyspark_Removing_Invalid_Rows.py
+++ Templates_data_processing/Pyspark_Removing_Invalid_Rows.py
@@ -38,3 +38,18 @@ print("Initial count: %d\nFinal count: %d" % (initial_count, final_count))
 +--------------------+--------+
 only showing top 20 rows
 '''
+
+# Determine the row counts for each DataFrame
+split_count = split_df.count()
+joined_count = joined_df.count()
+
+# Create a DataFrame containing the invalid rows
+invalid_df = split_df.join(F.broadcast(joined_df), 'folder', 'left_anti')
+
+# Validate the count of the new DataFrame is as expected
+invalid_count = invalid_df.count()
+print(" split_df:\t%d\n joined_df:\t%d\n invalid_df: \t%d" % (split_count, joined_count, invalid_count))
+
+# Determine the number of distinct folder rows removed
+invalid_folder_count = invalid_df.select('folder').distinct().count()
+print("%d distinct invalid folders found" % invalid_folder_count)
